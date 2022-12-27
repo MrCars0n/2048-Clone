19a940401625325cade98ebb009ac4855e5c2de8
Carson Kramer <95256790+MrCars0n@users.noreply.github.com>
2022-12-26 21:15:38 -0500

    Completed game and formatting

--- square.py
+++ square.py
@@ -1,8 +1,10 @@
+# Colors
 ColorEmptySq = (204, 192, 179)
 Color2 = (238, 228, 218)
 Color4 = (237, 224, 200)
 Color8 = (242, 177, 121)
 Color16 = (245, 149, 99)
+Color32 = (231, 130, 102)
 Color64 = (245, 124, 95)
 Color128 = (237, 206, 113)
 Color256 = (237, 204, 97)
@@ -11,6 +13,8 @@ Color1024 = (237, 197, 63)
 Color2048 = (238, 194, 45)
 ColorAbove2048 = (61, 58, 51)
 
+
+# Determine color by number
 def determineColor(number):
     if number == 0:
         return ColorEmptySq
@@ -22,6 +26,8 @@ def determineColor(number):
         return Color8
     elif number == 16:
         return Color16
+    elif number == 32:
+        return Color32
     elif number == 64:
         return Color64
     elif number == 128:
@@ -37,11 +43,18 @@ def determineColor(number):
     else:
         return ColorAbove2048
 
+
 class Square:
-    def __init__(self, number=0, width=0):
+    def __init__(self, number=0, width=0, color=0):
         self.number = number
         self.width = width
-        self.color = determineColor(number)
+
+        # Determine color based on number if not specified in constructor
+        if color == 0:
+            self.color = determineColor(number)
+        else:
+            self.color = color
+        self.hasCombined = False
 
     def setWidth(self, width):
         self.width = width
@@ -54,6 +67,12 @@ class Square:
 
     def getNumber(self):
         return self.number
+
+    def getHasCombined(self):
+        return self.hasCombined
+
+    def setHasCombined(self, boolean):
+        self.hasCombined = boolean
 
     def getColor(self):
         self.color = determineColor(self.number)
